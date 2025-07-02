ef53be6d61ebb29f6034e59c65c44c6e4384a14d
karolinyB <ana.silva@baymetrics.com.br>
2025-07-02 14:44:41 -0300

    chore: initial infrastructure and single-spa setup

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -33,6 +33,11 @@ declare module "*.webp" {
   export default src;
 }
 
+declare module "*.css" {
+  const src: string;
+  export default src;
+}
+
 declare module "*.svg" {
   const src: string;
   export default src;
