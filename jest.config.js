b94b0b5d140b785884cadf026a85d6d82b3ec549
karolinyB <ana.silva@baymetrics.com.br>
2025-07-07 16:26:12 -0300

    chore: configure Jest to handle imports of images and external libs

--- jest.config.js
+++ jest.config.js
@@ -4,8 +4,12 @@ module.exports = {
   transform: {
     "^.+\\.(j|t)sx?$": "babel-jest",
   },
+  transformIgnorePatterns: [
+    "/node_modules/(?!@material-tailwind/react|hex-rgb)",
+  ],
   moduleNameMapper: {
     "\\.(css)$": "identity-obj-proxy",
+    "\\.(jpg|jpeg|png|gif|webp|svg)$": "<rootDir>/__mocks__/fileMock.js",
     "single-spa-react/parcel": "single-spa-react/lib/cjs/parcel.cjs",
   },
   setupFilesAfterEnv: ["@testing-library/jest-dom"],
